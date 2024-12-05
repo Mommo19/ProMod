d7f09e70fbaf98f657ec8c63d2d25e2afb638ab7
Mommo19 <marius.bors.lind@gmail.com>
2024-12-05 12:11:25 +0100

    tyngdekraft

--- celest 0.1.py
+++ celest 0.1.py
@@ -8,6 +8,8 @@ Sort = (0, 0, 0)
 Hvit = (255, 255, 255)
 x = 200
 y = 200
+x_fart = 0
+y_fart = 0
 
 pygame.init()
 bakgrunnd = pygame.display.set_mode((1000,700))
@@ -19,21 +21,26 @@ pygame.display.update()
 
 
 while True:
+    
+    y_fart += 0.001
+    x += x_fart
+    y += y_fart
     for klik in pygame.event.get():
         if klik.type == pygame.QUIT:
             pygame.quit()
         if klik.type == pygame.KEYDOWN:
-            if klik.key == pygame.K_w:
-                y -= 20
-            if klik.key == pygame.K_s:
-                y += 20
+            if klik.key == pygame.K_SPACE:
+                y_fart -= 1
+#            if klik.key == pygame.K_s:
+#                y_fart += 20
             if klik.key == pygame.K_a:
-                x -= 20
+                x_fart -= 1
             if klik.key == pygame.K_d:
-                x += 20
+                x_fart += 1
             if klik.key == pygame.K_ESCAPE:
                 pygame.quit()
     bakgrunnd.fill(Hvit)
     pygame.draw.rect(bakgrunnd, Blå, (x, y, 50, 50))
+    pygame.draw.rect(bakgrunnd, Grønn, (0, 350, 1000, 50))
     pygame.display.update()
 pygame.quit()
